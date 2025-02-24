c0d9b817de519a55428333beab98cd11219a80c9
Viacheslav Pavlenko <Viacheslav.Pavlenko@infopulse.com>
2025-02-24 18:59:45 +0200

    second commit

---
+++ integer_test/pack.go
@@ -0,0 +1,5 @@
+package integertest
+
+func Add(x, y int) int {
+	return x + y
+}
---
+++ integer_test/pack_test.go
@@ -0,0 +1,27 @@
+package integertest
+
+import "testing"
+
+func TestAdder(t *testing.T) {
+
+	t.Run("test example", func(t *testing.T) {
+		sum := Add(2, 2)
+		expected := 4
+
+		if sum != expected {
+			t.Errorf("expected '%d' but got '%d' ", expected, sum)
+		}
+
+	})
+
+	t.Run("test example 2+3", func(t *testing.T) {
+		sum := Add(2, 3)
+		expected := 5
+
+		if sum != expected {
+			t.Errorf("expected '%d' but got '%d' ", expected, sum)
+		}
+
+	})
+
+}
